2f08a91c7d05362c7e0c1ea29c4fe064466450d1
Antônio <antonio.fernandes.de.santana.neto@gmail.com>
2022-02-02 22:15:12 -0300

    atualization

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import site2itnew from "./images/portfolio/site2itnew.png"
 import site2itold from "./images/portfolio/site2it_old.png"
 import portfolio from "./images/portfolio/portfolio.png"
 import netflix from "./images/portfolio/netflix.png"
+import loginUi from "./images/portfolio/loginUi.png"
+import escolinha from "./images/portfolio/escolinha.png"
 import iknowui from "./images/portfolio/ikui1.png"
 
 
@@ -112,8 +114,10 @@ export default function App() {
             <h2>Portifólio</h2>
             <div className="imgs">
               <a href="https://site2it.com" target="_blank" rel="noreferrer"><img alt="Site2it" src={site2itnew}/></a>
-              <a href="https://netflix-clone-ebon-sigma.vercel.app" target="_blank" rel="noreferrer"><img alt="Clone da Netflix" src={netflix}/></a>
+              <a href="https://learning-next-beryl.vercel.app" target="_blank" rel="noreferrer"><img alt="I know UI" src={escolinha}/></a>
+              <a href="https://loginui-azure.vercel.app" target="_blank" rel="noreferrer"><img alt="LoginUi" src={loginUi}/></a>
               <a href="https://i-know-ui.vercel.app" target="_blank" rel="noreferrer"><img alt="I know UI" src={iknowui}/></a>
+              <a href="https://netflix-clone-ebon-sigma.vercel.app" target="_blank" rel="noreferrer"><img alt="Clone da Netflix" src={netflix}/></a>
               <a href="https://antonionetodeveloper.github.io/Site2it/" target="_blank" rel="noreferrer"><img alt="antigo site2it" src={site2itold}/></a>
               <a href="https://antonionetodeveloper.github.io/BuscaCEP/" target="_blank" rel="noreferrer"><img alt="Buscador de CEP" src={buscaCEP}/></a>
               <a href="https://antonionetodeveloper.github.io/Portfolio/" target="_blank" rel="noreferrer"><img alt="Meu portifólio" src={portfolio}/></a>
